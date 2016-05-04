d0c5cc3729db3350add170d8ae2b4ad106f49053
Weapon45 <luisorlando.itsx@gmail.com>
2016-05-04 10:20:03 -0500

    04052016
    8

---
+++ js/actions.js
@@ -0,0 +1,22 @@
+var fn = {
+	ready: function(){
+		document.addEventListener("deviceready",fn.init,false);
+	},
+	init: function(){
+		// FUNCION PARA INICIO
+		window.location.href = '#login';
+		$('#BtnLogin').click(fn.Loguear);
+	},
+	Loguear: function(){
+		// FUNCION PARA LOGUEARSE
+		var nom = $('#user').val();
+		var passw = $('#pass').val();
+		if(nom != '' && passw != ''){
+			$.mobile.loading("show",{theme: 'b'});
+		}
+		else{
+			navigator.notification.alert("Todos Los Campos Son Requeridos",null,"Error al Ingresar","Aceptar");
+		}
+	}
+};
+$(fn.ready);
