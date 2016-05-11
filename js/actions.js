0c4c81d10f89439043a51c799c89208d3fe3f8a6
Weapon45 <luisorlando.itsx@gmail.com>
2016-05-11 13:45:27 -0500

    11052016

--- js/actions.js
+++ js/actions.js
@@ -86,10 +86,8 @@ var fn = {
 	leerLpn: function(){
 		cordova.plugins.barcodeScanner.scan(
 		  function (result) {
-			  alert("We got a barcode\n" +
-					"Result: " + result.text + "\n" +
-					"Format: " + result.format + "\n" +
-					"Cancelled: " + result.cancelled);
+			  //alert("Result: " + result.text);
+			  navigator.notification.alert("Resultado: " + result.text,null,"Felicidades","Aceptar");
 		  }, 
 		  function (error) {
 			  alert("Scanning failed: " + error);
