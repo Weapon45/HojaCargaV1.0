7a07f9f39b74ec2e0d4e4263d8d3bd431067452e
Weapon45 <luisorlando.itsx@gmail.com>
2016-05-12 14:26:03 -0500

    12052016

--- js/actions.js
+++ js/actions.js
@@ -95,19 +95,19 @@ var fn = {
 	leerLpn: function(){
 		cordova.plugins.barcodeScanner.scan(
 		  function (result) {
+			  $.mobile.loading("show",{theme: 'b'});
 			  var lpns = result.text; 
 			  $.ajax({
 				method: 'POST',
 				url: 'http://servidoriis.laitaliana.com.mx/OV/ServicesHC/HC.asmx/Datos',
 				data: {lpn: lpns},
 				dataType: "json",
-				success: function (msg){	
-					$.mobile.loading("show",{theme: 'b'});
+				success: function (msg){						
 					//alert(JSON.stringify(msg));
 					$.each(msg, function(i, item){
 						//alert(msg[i].NUM);
 						$.mobile.loading("hide");
-						navigator.notification.alert("compañia: " + msg[i].COMPANY_CODE + "division: " + msg[i].DIVISION + "lpn: " + msg[i].DC_LICENSE_PLATE_ID + "almacen: " + msg[i].WAREHOUSE + "codigo: " + msg[i].PART_CODE + "descripcion: " + msg[i].DESCRIPCION + "cantidad: " + msg[i].CANTIDAD + "unidad: " + msg[i].UOM_1 + "lote: " + msg[i].IC_LOT_NUMBER + "nota: " + msg[i].DESPATCH_NOTE,null,"Felicidades Culero","Aceptar");
+						navigator.notification.alert("compañia: " + msg[i].COMPANY_CODE + "division: " + msg[i].DIVISION + "lpn: " + msg[i].DC_LICENSE_PLATE_ID + "almacen: " + msg[i].WAREHOUSE + "codigo: " + msg[i].PART_CODE + "descripcion: " + msg[i].DESCRIPCION + "cantidad: " + msg[i].CANTIDAD + "unidad: " + msg[i].UOM_1 + "lote: " + msg[i].IC_LOT_NUMBER + "nota: " + msg[i].DESPATCH_NOTE,null,"Felicidades","Aceptar");
 					});
 				},
 				error: function(jq, txt){
