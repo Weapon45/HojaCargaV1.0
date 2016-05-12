01926abfbb41d9af2e2031650b7a5ae4726db120
Weapon45 <luisorlando.itsx@gmail.com>
2016-05-12 16:47:58 -0500

    12052016

--- js/almacenamiento.js
+++ js/almacenamiento.js
@@ -1,5 +1,9 @@
 var HC = {
-	pc: null,
+	partcodehc: null,
+	descriptionhc: null,
+	lpnhc: null,
+	lotehc: null,
+	cantidadhc: null,
 	leerLpn: function(){
 		cordova.plugins.barcodeScanner.scan(
 		  function (result) {
@@ -15,7 +19,7 @@ var HC = {
 					$.each(msg, function(i, item){
 						//alert(msg[i].NUM);
 						$.mobile.loading("hide");
-						HC.guardarLineas(msg[i].PART_CODE);
+						HC.guardarLineas(msg[i].PART_CODE,msg[i].DESCRIPCION,msg[i].DC_LICENSE_PLATE_ID,msg[i].IC_LOT_NUMBER,msg[i].CANTIDAD + " " + msg[i].UOM_1);
 						//navigator.notification.alert("compañia: " + msg[i].COMPANY_CODE + "division: " + msg[i].DIVISION + "lpn: " + msg[i].DC_LICENSE_PLATE_ID + "almacen: " + msg[i].WAREHOUSE + "codigo: " + msg[i].PART_CODE + "descripcion: " + msg[i].DESCRIPCION + "cantidad: " + msg[i].CANTIDAD + "unidad: " + msg[i].UOM_1 + "lote: " + msg[i].IC_LOT_NUMBER + "nota: " + msg[i].DESPATCH_NOTE,null,"Felicidades","Aceptar");
 					});
 				},
@@ -31,8 +35,12 @@ var HC = {
 		  }
 	   );
 	},
-	guardarLineas: function(pc){
-		HC.pc = pc;
-		navigator.notification.alert("Hola Si Entro");
+	guardarLineas: function(partcodehc,descriptionhc,lpnhc,lotehc,cantidadhc){
+		HC.partcodehc = partcodehc;
+		HC.descriptionhc = descriptionhc;
+		HC.lpnhc = lpnhc;
+		HC.lotehc = lotehc;
+		HC.cantidadhc = cantidadhc;
+		navigator.notification.alert(HC.partcodehc + " " + HC.descriptionhc + " " + HC.lpnhc + " " + HC.lotehc + " " + HC.cantidadhc);
 	}
 }
