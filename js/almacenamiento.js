3e7b757705d081620ec3e993a7314ba01794f15b
Weapon45 <luisorlando.itsx@gmail.com>
2016-05-13 13:23:37 -0500

    13052016

--- js/almacenamiento.js
+++ js/almacenamiento.js
@@ -16,8 +16,8 @@ var HC = {
 						
 						HC.db = window.openDatabase("hcApp","1.0","HojaCargaApp Storage",20000);
 						HC.db.transaction(function(tx){
-							tx.executeSql("CREATE TABLE IF NOT EXISTS datos (d1,d2,d3,d4,d5)");
-							tx.executeSql("INSERT INTO datos (d1,d2,d3,d4,d5) VALUES ('" + msg[i].PART_CODE + "','" + msg[i].DESCRIPCION + "','" + msg[i].DC_LICENSE_PLATE_ID + "','" + msg[i].IC_LOT_NUMBER + "','" + msg[i].CANTIDAD + "')");
+							tx.executeSql("CREATE TABLE IF NOT EXISTS datos (d1,d2,d3,d4,d5,d6)");
+							tx.executeSql("INSERT INTO datos (d1,d2,d3,d4,d5,d6) VALUES ('" + msg[i].PART_CODE + "','" + msg[i].DESCRIPCION + "','" + msg[i].DC_LICENSE_PLATE_ID + "','" + msg[i].IC_LOT_NUMBER + "','" + msg[i].CANTIDAD + "','" + msg[i].UOM_1 + "')");
 							alert(msg[i].PART_CODE + " " + msg[i].DESCRIPCION + " " + msg[i].DC_LICENSE_PLATE_ID + " " + msg[i].IC_LOT_NUMBER + " " + msg[i].CANTIDAD + " " + msg[i].UOM_1);
 						},function(){
 							alert("Error insertando datos");
@@ -47,20 +47,14 @@ var HC = {
 			var tabla = '<li data-role="list-divider"><center><h1>CONTENIDO HOJA CARGA<h1></center></li>';			
 			for(i = 0; i < t.rows.length; i++){
 					x = x + 1;	
-				tabla += '<li><h5>LINEA ' + x + '</h5><ul><li>' + t.rows.item(i).d1 + '</li><li>' + t.rows.item(i).d2 + '</li><li>' + t.rows.item(i).d3 + '</li><li>' + t.rows.item(i).d4 + '</li><li>' + t.rows.item(i).d5 + '</li></ul></li>';
+				tabla += '<li ><h5>LINEA ' + x + '</h5><ul><li>' + t.rows.item(i).d1 + '</li><li>' + t.rows.item(i).d2 + '</li><li>' + t.rows.item(i).d3 + '</li><li>' + t.rows.item(i).d4 + '</li><li>' + t.rows.item(i).d5 + " " + t.rows.item(i).d6 + '</li></ul></li>';
 			}
 			$("#lin").html(tabla);
 		});
 	},
 	error: function(){
-		//alert("error base de datos");
-		navigator.notification.alert("Error Base de Datos",null,"Error BD","Aceptar");
-		//var tabla = '<ul data-role="listview" data-inset="true"><li data-role="list-divider"><center>LECTURA DE LPNS</center></li>';
-		//tabla += '<li><a href="#" id="btnNewLPN">Nueva LPN</a></li><li><a href="#consultaLineas" id="HistorialCapturado">Lineas Hoja Carga</a></li>';
-		//tabla += '</ul>'
-		//	$("#lin").html(tabla);
-		//var tabla = '<li data-role="list-divider"><center><h1>CONTENIDO HOJA CARGA<h1></center></li>';
-		//tabla += '<li><h5>LINEA</h5><ul><li>sjvhsdf</li><li>sfdajasda</li><li>bncda</li></ul></li>';
+		navigator.notification.alert("Error Base de Datos",null,"Error BD","Aceptar");		
+		//var tabla = '<ul data-role="listview" data-inset="true"><li data-role="list-divider"><center>LINEAS HOJA CARGA</center></li></ul>';
 		//$("#lin").html(tabla);
 	}
 }
